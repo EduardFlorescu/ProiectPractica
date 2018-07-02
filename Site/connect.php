96f1e0ca7fb1f58627cab1c7e76ed7b71db0e5d8
Mihaela Ciobanu <ciobanu.miha.dorina@gmail.com>
2018-07-02 13:35:06 +0300

    Adaugare connect.php

---
+++ Site/connect.php
@@ -0,0 +1,9 @@
+<?php
+require_once "config.php";
+//Stabiliesc conexiunea cu serverul MySQL
+$conexiune = mysqli_connect(DB_HOST, DB_USER, DB_PASS);
+if (!$conexiune) {
+ die('Eroare conectare la MySQL: ' . mysqli_connect_error());
+}
+mysqli_select_db($conexiune, DB_NAME) or die("Eroare la selectarea bazei de date " . mysqli_error($conexiune));
+?>
