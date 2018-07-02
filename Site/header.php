f96aa5f637a8368631075591005b66bb4adb6791
Mihaela Ciobanu <ciobanu.miha.dorina@gmail.com>
2018-07-02 11:49:19 +0300

    Adaugare header.php

---
+++ Site/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Restaurant</title>
+</head>
+<body>
+    <div id="container">
+        <div id="header">
+            <div id="logo">
+                <h1>
+                    !!!Restaurant!!!
+                </h1>
+            </div>
+
+            <div id="navigation-bar">
+                <a href="index.php">HOME</a>
+                <a href="meniu.php">MENIU</a>
+                <a href="contact.php">CONTACT</a>
+                <a href="login.php">LOG IN</a>
+            </div>
+
+            <img src="./img/restaurant.jpg" alt="Restaurant"/>
+        </div>
+    
