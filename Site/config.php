d285ed4c36dcdd751dc71751b2e34ae74444f4e6
Mihaela Ciobanu <ciobanu.miha.dorina@gmail.com>
2018-07-02 13:34:11 +0300

    Adaugare config.php

---
+++ Site/config.php
@@ -0,0 +1,8 @@
+<?php
+//Stabilire detalii conexiune
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'RestaurantDB');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
+?>
