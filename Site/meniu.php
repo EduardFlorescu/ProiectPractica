3f5f29709041312a79db6fe207562c56d8ceef57
Mihaela Ciobanu <ciobanu.miha.dorina@gmail.com>
2018-07-02 13:36:07 +0300

    Conectare la baza de date in meniu.php

--- Site/meniu.php
+++ Site/meniu.php
@@ -1,8 +1,21 @@
 <?php
-include "header.php"
+include "header.php";
+include "connect.php";
 ?>
 
+<?php
+    $query = "Select * from Meniu";
+    $result = mysqli_query($conexiune, $query);
 
+    if(mysqli_num_rows($result)){
+        while($row=mysqli_fetch_array($result)){
+            //print("<img src=&quot;date:image/jpeg;base64,".base64_encode($row['Img']).">");
+            echo '<img src="data:image/jpeg;base64,'.base64_encode($row['Img']).'"height="100" weight="100"/> <br/>';
+        }
+    }else{
+        print("Nu exista intrari in baza de date!");
+    }
+?>
 
 <?php
 include "footer.php"
