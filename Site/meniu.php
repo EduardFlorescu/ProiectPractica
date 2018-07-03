79f1b51bbd63bcf4d64e6b1cd10cd3ec3a6135eb
Mihaela Ciobanu <ciobanu.miha.dorina@gmail.com>
2018-07-03 14:12:25 +0300

    Adaugare meniurilor in tabel

--- Site/meniu.php
+++ Site/meniu.php
@@ -2,21 +2,23 @@
 include "header.php";
 include "connect.php";
 ?>
-
+<div id="meniuri">
 <?php
     $query = "Select * from Meniu";
     $result = mysqli_query($conexiune, $query);
 
     if(mysqli_num_rows($result)){
+        print('<br/><table align="center"><tr>');
         while($row=mysqli_fetch_array($result)){
-            //print("<img src=&quot;date:image/jpeg;base64,".base64_encode($row['Img']).">");
-            echo '<img src="data:image/jpeg;base64,'.base64_encode($row['Img']).'"height="100" weight="100"/> <br/>';
+           echo '<td><a href="produse.php"><img src="data:image/jpeg;base64,'.base64_encode($row['Img']).
+            '"height="100" weight="100"/><div class="centered">'.$row['Nume'].'</div></a></td>';
         }
+        print('</tr></table>');
     }else{
         print("Nu exista intrari in baza de date!");
     }
 ?>
-
+</div>
 <?php
 include "footer.php"
  ?>
