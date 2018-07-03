48ba1cf3dcc016abd58800b15bbe752ce4c0488e
Mihaela Ciobanu <ciobanu.miha.dorina@gmail.com>
2018-07-03 14:10:43 +0300

    Adaugare butoane in bara de navigatie

--- Site/header.php
+++ Site/header.php
@@ -3,23 +3,27 @@
 <head>
     <meta charset="utf-8" />
     <title>Restaurant</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+  
+
+    <link rel="stylesheet" href="../CSS/style.css"
 </head>
 <body>
     <div id="container">
         <div id="header">
             <div id="logo">
-                <h1>
-                    !!!Restaurant!!!
-                </h1>
+                <img src="../Img/logo.gif" alt="HILMAD"/>
             </div>
 
             <div id="navigation-bar">
-                <a href="index.php">HOME</a>
-                <a href="meniu.php">MENIU</a>
-                <a href="contact.php">CONTACT</a>
-                <a href="login.php">LOG IN</a>
+                <a href="index.php"><button type="button" class="btn btn-success">HOME</button></a>
+                <a href="meniu.php"><button type="button" class="btn btn-success">MENIU</button></a>
+                <a href="contact.php"><button type="button" class="btn btn-success">CONTACT</button></a>
+                <a href="login.php"><button type="button" class="btn btn-success">LOG IN</button></a>
             </div>
-
-            <img src="../img/restaurant.jpg" alt="Restaurant"/>
+            
+            <img id="imgLogo" src="../img/restaurant.jpg" alt="Restaurant"/>
+            
+            
         </div>
     
