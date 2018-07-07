fcf6010103edf85402be9d4f5c44643626f15765
Mihaela Ciobanu <ciobanu.miha.dorina@gmail.com>
2018-07-07 22:23:00 +0300

    Modificare baza de date - impartirea bazei in 4 tabele

--- BazaDeDate/RestaurantDB.sql
+++ BazaDeDate/RestaurantDB.sql
@@ -1,15 +1,36 @@
-CREATE TABLE Meniu(
-	Id INT PRIMARY KEY NOT NULL,
-	Nume VARCHAR(50)
+CREATE DATABASE restaurantdb;
+
+CREATE TABLE meniu(
+	idMeniu INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	numeCategorie VARCHAR(50),
+	caleImagineCategorie VARCHAR(100)
 );
 
-CREATE TABLE DetaliiMeniu(
-	Id INT,
-	Tip VARCHAR(50) PRIMARY KEY,
-	Pret FLOAT,
-	Ingrediente VARCHAR(50)
+CREATE TABLE detaliiMeniu(
+	idMeniu INT,
+	idProdus INT
+);
+CREATE TABLE produse(
+    idProdus INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	numeProdus VARCHAR(50),
+	pret DECIMAL(8,2),
+	gramaj DECIMAL(8,2),
+	ingrediente VARCHAR(300),
+	caleImagineProdus VARCHAR(100)
 );
 
-ALTER TABLE DetaliiMeniu
-ADD CONSTRAINT FK_Id
-FOREIGN KEY (Id) REFERENCES Meniu(Id);
+ALTER TABLE detaliiMeniu
+ADD CONSTRAINT FK_Id_Categorie
+FOREIGN KEY (idMeniu) REFERENCES meniu(idMeniu);
+
+ALTER TABLE detaliiMeniu
+ADD CONSTRAINT FK_Id_Produs
+FOREIGN KEY (idProdus) REFERENCES produse(idProdus);
+
+CREATE TABLE cont(
+	idCont INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	nume VARCHAR(50),
+	prenume VARCHAR(50),
+	username VARCHAR(50),
+	password VARCHAR(50)
+);
