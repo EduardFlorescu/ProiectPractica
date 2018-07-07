f235da9fe9e7e99edde774f8cdd6b08d1e3c1bdd
Mihaela Ciobanu <ciobanu.miha.dorina@gmail.com>
2018-07-07 22:26:04 +0300

    Redenumirea bazei de date in fisierul de configurare

--- Site/config.php
+++ Site/config.php
@@ -1,7 +1,7 @@
 <?php
 //Stabilire detalii conexiune
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'RestaurantDB');
+define('DB_NAME', 'restaurantdb');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
