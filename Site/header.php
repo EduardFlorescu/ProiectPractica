74c73e88cdd2846a5a30423036071205461a6e66
Mihaela Ciobanu <ciobanu.miha.dorina@gmail.com>
2018-07-07 22:32:18 +0300

    Verificarea daca exista o sesiune sau nu - daca este conectat un user

--- Site/header.php
+++ Site/header.php
@@ -1,4 +1,8 @@
-<!DOCTYPE html>
+<?php
+    include "connect.php";
+    session_start();
+    //require_once("connect.php")
+?>
 <html>
 <head>
     <meta charset="utf-8" />
@@ -12,17 +16,42 @@
     <div id="container">
         <div id="header">
             <div id="logo">
-                <img src="../Img/logo.gif" alt="HILMAD"/>
+                <h1>HILMAD</h1>
             </div>
 
             <div id="navigation-bar">
                 <a href="index.php"><button type="button" class="btn btn-success">HOME</button></a>
                 <a href="meniu.php"><button type="button" class="btn btn-success">MENIU</button></a>
                 <a href="contact.php"><button type="button" class="btn btn-success">CONTACT</button></a>
-                <a href="login.php"><button type="button" class="btn btn-success">LOG IN</button></a>
+                <?php
+                if (isset($_SESSION['username']) && isset($_SESSION['password'])) {
+                     // preia datele din sesiune
+                    $username = $_SESSION['username'];
+                    $password = $_SESSION['password'];
+                
+                    // formeaza si executa query-ul de select din baza de date
+                    $query = "SELECT * FROM cont WHERE `username` = '".$username."' AND `password` = '".$password."'";
+                    $result = mysqli_query($conexiune,$query) or die ( "Error : ". mysqli_error($conexiune) );
+                
+                    // verifica daca interogarea MySQL a gasit date valide
+                    if (mysqli_num_rows($result) < 1) {
+                        // daca nu, afiseaza un mesaj de eroare
+                        $ok=0;
+                        echo '<a href="login.php"><button type="button" class="btn btn-success">LOG IN</button></a>';
+                    } else {
+                        echo '<a href="signout.php"><button type="button" class="btn btn-success">SIGN OUT</button></a>';
+                        $ok=1;
+                    }
+                        
+                }else{
+                    echo '<a href="login.php"><button type="button" class="btn btn-success">LOG IN</button></a>';
+                    $ok=0;
+                } 
+                ?>
+                
             </div>
             
-            <img id="imgLogo" src="../img/restaurant.jpg" alt="Restaurant"/>
+            <img id="imgLogo" src="../Imagini/restaurant.jpg" alt="Restaurant"/>
             
             
         </div>
