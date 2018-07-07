1bddf0127b81bb1281b28be2223f34165ea4a5cc
Mihaela Ciobanu <ciobanu.miha.dorina@gmail.com>
2018-07-07 22:27:04 +0300

    Adaugare continut in pagina index.html

--- Site/index.php
+++ Site/index.php
@@ -2,8 +2,16 @@
 include "header.php"
 ?>
 
-<p></p>
+<div id="content">
+    <p>Un mix exceptional intre bucataria moderna si cea clasica, un mix ce include specialitati mediteraneene si preparate traditionale romanesti.</p>
+    <p>Elegantul restaurant HILMAD este locul potrivit pentru o calatorie gastronomica fascinanta in urma careia papilele dvs. gustative vor implora sa se intoarca.</p>
+    <p>Restaurantul este ideal atat pentru momente speciale cat si pentru evenimente personale. O nunta intima doar cu prietenii apropiati, un botez restrans sau o petrecere in familie, toate pot prinde un contur de basm in locatia noastra de poveste.</p>
+    <h3>FACILITATI RESTAURANT</h3>
+    <p>Restaurantul are o capacitate de 120 de locuri, iar in ceea ce priveste evenimentele, aranjamentul meselor depinde in functie de numarul de persoane si de preferintele dvs. Sala este dotata cu echipament audio, videoproiector si ecran de proiectie, si in plus, punem la dispozitie alte materiale de logistica cum ar fi flipchart-uri.</p>
+    <p>Pe langa sala cea mare, dispunem si de o incapere cu o capacitate de 35-40 de locuri pentru diverse intalniri sau pentru conferinte mai mici.</p>
+    <p>Terasa situata in curtea destul de spatioasa va sta la dispozitie pentru a servi masa in adierea blanda a vantului sau pentru a va bucura din plin de atingerea delicata a soarelui.</p>
+</div>
 
 <?php
 include "footer.php"
- ?>
+?>
