b182dcf7e4dad8d70ea5fd934cacd0ce867faa7b
Mihaela Ciobanu <ciobanu.miha.dorina@gmail.com>
2018-07-07 22:30:00 +0300

    Vizualizarea categoriilor din baza de date

--- Site/meniu.php
+++ Site/meniu.php
@@ -1,23 +1,60 @@
 <?php
 include "header.php";
-include "connect.php";
 ?>
-<div id="meniuri">
-<?php
-    $query = "Select * from Meniu";
-    $result = mysqli_query($conexiune, $query);
-
-    if(mysqli_num_rows($result)){
-        print('<br/><table align="center"><tr>');
-        while($row=mysqli_fetch_array($result)){
-           echo '<td><a href="produse.php"><img src="data:image/jpeg;base64,'.base64_encode($row['Img']).
-            '"height="100" weight="100"/><div class="centered">'.$row['Nume'].'</div></a></td>';
+<div id="content">
+    <div id="btn">
+    <?php if($ok==1)
+            echo '<a href="add_meniu.php"><button type="button" class="btn btn-success">ADD</button></a>';
+        ?>
+    <div>
+    <div id="meniuri">
+    <?php
+    $comanda = $_REQUEST["comanda"];
+    if (isset($comanda)) {
+        switch ($comanda){
+            case 'delete':
+                $id = $_REQUEST["delete"];
+                $sql = "DELETE FROM meniu WHERE idMeniu='$id'";
+                if (!mysqli_query($conexiune, $sql)) {
+                    die('Error: ' . mysqli_error($conexiune));
+                }
+                echo "<font color='red'>Intrarea cu id-ul $id a fost stearsa cu succes</font><br/>";
+                break;
         }
-        print('</tr></table>');
-    }else{
-        print("Nu exista intrari in baza de date!");
+
     }
+
 ?>
+    
+    <?php
+        $query = "Select * from meniu";
+        $result = mysqli_query($conexiune, $query);
+
+        if(mysqli_num_rows($result)){
+            print('<br/><table align="center"><tr>');
+            while($row=mysqli_fetch_array($result)){
+            echo '<td><form action="produse.php"><button type="submit" name="categorie" value="'.$row['idMeniu'].'">
+            <img src="'.$row['caleImagineCategorie'].'" alt="'.$row['numeCategorie'].'" height="100" weight="100" ></button>
+            <div class="centered">'.$row['numeCategorie'].'</div></a>    </form>';
+    ?>
+
+     <?php if($ok==1)
+    echo '<form action="meniu.php" method="post">
+      <input name="comanda" type="hidden" value="delete" />
+      <input type="hidden" name="delete" value="'.$row["idMeniu"].'"/>
+        <input type="submit" value="Delete" />
+    </form>';
+    ?>
+    
+    <?php
+      echo '</td>';
+            }
+            print('</tr></table>');
+        }else{
+            print("Nu exista intrari in baza de date!");
+        }
+    ?>
+    </div>
 </div>
 <?php
 include "footer.php"
