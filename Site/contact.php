562edda57373d7f19eb8ad6a4857cb82de7c7dfb
Mihaela Ciobanu <ciobanu.miha.dorina@gmail.com>
2018-07-07 22:28:05 +0300

    Adaugare continut in pagina contact.php

--- Site/contact.php
+++ Site/contact.php
@@ -1,7 +1,12 @@
 <?php
 include "header.php"
 ?>
-
+<div id="content">
+    <p>Adresa: Str. Amaradia nr. 4, Craiova, Dolj, Romania<br/>
+    Telefon: +40372.981.433 +40770.109.390<br/>
+    Email: hilmad@gmail.com<br/>
+    </p>
+</div>
 <?php
 include "footer.php"
  ?>
