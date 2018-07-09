ef1f8a932cc956060c5a22d1f42e11167628168f
Florescu Eduard <fl_eduard@yahoo.com>
2018-07-09 16:55:59 +0300

    Am modificat si stilizat header-ul si footer-ul adaugand referintele de la css.

--- Site/footer.php
+++ Site/footer.php
@@ -1,3 +1,25 @@
+<!-- Contact Section -->
+<div id="contact" class="text-center">
+  <div class="overlay">
+    <div class="container">
+      <div class="col-md-8 col-md-offset-2">
+        <div class="social">
+          <ul>
+            <li><a href="https://www.facebook.com/"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="https://twitter.com/"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://www.instagram.com/"><i class="fa fa-instagram"></i></a></li>
+          </ul>
+        </div>
+      </div>
     </div>
-  </body>
+  </div>
+</div>
+<div id="footer">
+  <div class="container text-center">
+    <div class="fnav">
+      <p>Copyright &copy; 2018 HILMAD.</p>
+    </div>
+  </div>
+</div>
+</body>
 </html>
--- Site/header.php
+++ Site/header.php
@@ -1,58 +1,94 @@
+<!DOCTYPE html>
 <?php
     include "connect.php";
     session_start();
     //require_once("connect.php")
 ?>
-<html>
+<html lang="en">
 <head>
-    <meta charset="utf-8" />
-    <title>Restaurant</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
-  
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>HILMAD-Restaurant</title>
+<!-- Favicons
+    ================================================== -->
+<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
 
-    <link rel="stylesheet" href="../CSS/style.css"
+<!-- Bootstrap -->
+<link rel="stylesheet" type="text/css"  href="../css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="../fonts/font-awesome/css/font-awesome.css">
+
+<!-- Stylesheet
+    ================================================== -->
+<link rel="stylesheet" type="text/css"  href="../css/style.css">
+<link rel="stylesheet" type="text/css" href="../css/prettyPhoto.css">
+<link href='http://fonts.googleapis.com/css?family=Lato:400,700,900,300' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800,600,300' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="../js/modernizr.custom.js"></script>
+
+
 </head>
-<body>
-    <div id="container">
-        <div id="header">
-            <div id="logo">
-                <h1>HILMAD</h1>
-            </div>
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
 
-            <div id="navigation-bar">
-                <a href="index.php"><button type="button" class="btn btn-success">HOME</button></a>
-                <a href="meniu.php"><button type="button" class="btn btn-success">MENIU</button></a>
-                <a href="contact.php"><button type="button" class="btn btn-success">CONTACT</button></a>
-                <?php
+<!-- Header -->
+<header id="header">
+  <div class="intro text-center">
+    <div class="overlay">
+      <div class="container">
+        <div class="row">
+          <div class="intro-text">
+            <h1>Hilmad</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+
+<!-- Navigation -->
+<div id="nav">
+  <nav class="navbar navbar-custom">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse"> <i class="fa fa-bars"></i> </button>
+      </div>
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse navbar-main-collapse">
+        <ul class="nav navbar-nav">
+          <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+          <li class="hidden"> <a href="#page-top"></a> </li>
+          <li> <a class="page-scroll" href="index.php">Home</a> </li>
+          <li> <a class="page-scroll" href="meniu.php" >Meniu</a> </li>
+          <li> <a class="page-scroll" href="contact.php">Contact</a> </li>
+          <?php
                 if (isset($_SESSION['username']) && isset($_SESSION['password'])) {
                      // preia datele din sesiune
                     $username = $_SESSION['username'];
                     $password = $_SESSION['password'];
-                
+
                     // formeaza si executa query-ul de select din baza de date
                     $query = "SELECT * FROM cont WHERE `username` = '".$username."' AND `password` = '".$password."'";
                     $result = mysqli_query($conexiune,$query) or die ( "Error : ". mysqli_error($conexiune) );
-                
+
                     // verifica daca interogarea MySQL a gasit date valide
                     if (mysqli_num_rows($result) < 1) {
                         // daca nu, afiseaza un mesaj de eroare
                         $ok=0;
-                        echo '<a href="login.php"><button type="button" class="btn btn-success">LOG IN</button></a>';
+                        echo '<li><a class="page-scroll" href="login.php">LOG IN</a></li>';
                     } else {
-                        echo '<a href="signout.php"><button type="button" class="btn btn-success">SIGN OUT</button></a>';
+                        echo '<li><a class="page-scroll" href="signout.php">SIGN OUT</a></li>';
                         $ok=1;
                     }
-                        
+
                 }else{
-                    echo '<a href="login.php"><button type="button" class="btn btn-success">LOG IN</button></a>';
+                    echo '<li><a class="page-scroll" href="login.php">LOG IN</a></li>';
                     $ok=0;
-                } 
+                }
                 ?>
-                
-            </div>
-            
-            <img id="imgLogo" src="../Imagini/restaurant.jpg" alt="Restaurant"/>
-            
-            
-        </div>
-    
+        </ul>
+      </div>
+    </div>
+  </nav>
+</div>
