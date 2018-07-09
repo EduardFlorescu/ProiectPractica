546a40d2933a9a6adc1863382f230d4778ac4c05
Florescu Eduard <fl_eduard@yahoo.com>
2018-07-09 17:05:19 +0300

    Am modificat index.php adaugand o imagine si modificand reprezentarea textului.

--- Site/index.php
+++ Site/index.php
@@ -1,17 +1,32 @@
 <?php
-include "header.php"
+    include "header.php";
 ?>
+<!-- About Section -->
+<div id="about">
+  <div class="container">
+    <div class="section-title text-center center">
+      <h2>About us</h2>
+      <hr>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-md-6"> <img src="../Imagini/despre.jpg" class="img-responsive" alt=""> </div>
+      <div class="col-xs-12 col-md-6">
+        <div class="about-text">
+          <p>Un mix exceptional intre bucataria moderna si cea clasica, un mix ce include specialitati mediteraneene si preparate traditionale romanesti.
 
-<div id="content">
-    <p>Un mix exceptional intre bucataria moderna si cea clasica, un mix ce include specialitati mediteraneene si preparate traditionale romanesti.</p>
-    <p>Elegantul restaurant HILMAD este locul potrivit pentru o calatorie gastronomica fascinanta in urma careia papilele dvs. gustative vor implora sa se intoarca.</p>
-    <p>Restaurantul este ideal atat pentru momente speciale cat si pentru evenimente personale. O nunta intima doar cu prietenii apropiati, un botez restrans sau o petrecere in familie, toate pot prinde un contur de basm in locatia noastra de poveste.</p>
-    <h3>FACILITATI RESTAURANT</h3>
-    <p>Restaurantul are o capacitate de 120 de locuri, iar in ceea ce priveste evenimentele, aranjamentul meselor depinde in functie de numarul de persoane si de preferintele dvs. Sala este dotata cu echipament audio, videoproiector si ecran de proiectie, si in plus, punem la dispozitie alte materiale de logistica cum ar fi flipchart-uri.</p>
-    <p>Pe langa sala cea mare, dispunem si de o incapere cu o capacitate de 35-40 de locuri pentru diverse intalniri sau pentru conferinte mai mici.</p>
-    <p>Terasa situata in curtea destul de spatioasa va sta la dispozitie pentru a servi masa in adierea blanda a vantului sau pentru a va bucura din plin de atingerea delicata a soarelui.</p>
+Elegantul restaurant HILMAD este locul potrivit pentru o calatorie gastronomica fascinanta in urma careia papilele dvs. gustative vor implora sa se intoarca.
+
+Restaurantul este ideal atat pentru momente speciale cat si pentru evenimente personale. O nunta intima doar cu prietenii apropiati, un botez restrans sau o petrecere in familie, toate pot prinde un contur de basm in locatia noastra de poveste.</p>
+          <p>Restaurantul are o capacitate de 120 de locuri, iar in ceea ce priveste evenimentele, aranjamentul meselor depinde in functie de numarul de persoane si de preferintele dvs. Sala este dotata cu echipament audio, videoproiector si ecran de proiectie, si in plus, punem la dispozitie alte materiale de logistica cum ar fi flipchart-uri.
+
+Pe langa sala cea mare, dispunem si de o incapere cu o capacitate de 35-40 de locuri pentru diverse intalniri sau pentru conferinte mai mici.
+
+Terasa situata in curtea destul de spatioasa va sta la dispozitie pentru a servi masa in adierea blanda a vantului sau pentru a va bucura din plin de atingerea delicata a soarelui.</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
-
 <?php
-include "footer.php"
+  include "footer.php";
 ?>
